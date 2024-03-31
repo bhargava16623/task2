60ac140d244c4e084b19ff72e2be9e4db1c8508f
bhargava16623 <105556187+bhargava16623@users.noreply.github.com>
2024-03-31 13:00:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function getUsers() {
+
+    const data = await fetch('https://reqres.in/api/users?page=1');
+    const json = await data.json();
+    setUsers(json.data);
+    console.log(json.data);
+
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='frame' >
+        <h1>Apple Users</h1>
+
+        <button onClick={()=>{ setLoading(true); setTimeout(()=>{  getUsers();
+        
+      },2000);}}>Get Users</button>
+
+      
+         
+        {loading ? 
+        <div id='box'>
+          <div id='shimmer'>
+             <h1>loading..</h1> 
+          </div>
+          <div id='shimmer'>
+             <h1>loading..</h1> 
+          </div>
+          <div id='shimmer'>
+             <h1>loading..</h1> 
+          </div>
+        </div>
+       : 
+        
+        <div id='box'>{
+        users?.map((user)=>{
+
+          return(
+          
+            <div id="container">
+              <img src={user.avatar}/>
+              <h2>First Name: {user.first_name}</h2>
+              <h2>Last Name: {user.last_name}</h2>
+              <h2>Email: {user.email}</h2>
+            </div>
+          
+          )
+        })
+
+      }</div>
+
+      }
+         
+         
+
     </div>
   );
 }
